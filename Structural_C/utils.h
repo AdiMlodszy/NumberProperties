20c3db175a058ef3318d5dcd830a8ea2f3cf2133
addktiv <adriankasprzakschool@gmail.com>
2025-05-08 20:29:31 +0200

    Feat: Add utils

---
+++ Structural_C/utils.h
@@ -0,0 +1,13 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <stdbool.h>
+
+// Deklaracje funkcji sprawdzających właściwości liczb
+bool is_palindromic(int number);
+bool is_perfect(int number);
+bool is_harshad(int number);
+bool is_happy(int number);
+bool is_armstrong(int number);
+
+#endif // UTILS_H
